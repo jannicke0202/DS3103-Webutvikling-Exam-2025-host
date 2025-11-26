bfe43d199c9e0b207e9626558f9e39470f547917
jannicke0202 <yangkikhangsar@gmail.com>
2025-11-26 13:22:47 +0100

    webapi oppsett dotnet og gitignore

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace DS3103_Webutvikling_Exam_2025_host;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
