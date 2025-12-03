41fd7e747729020c6dfd54be5cf53acd276a2663
Mikael <miga008@student.kristiania.no>
2025-12-03 13:24:06 +0100

    statisk tekst på homepage

---
+++ sportsworld-frontend/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+    return (
+        <>
+        <header>
+            <h1>SportsWorld</h1>
+            <p>Welcome to SportsWorld - Info about football-players</p>
+        </header>
+        
+        
+        </>
+    )
+}
+
+export default HomePage
---
+++ sportsworld-frontend/src/routing/AppRouting.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { HomePage } from "../pages";
+
+const AppRouting = () => {
+    return (
+        <BrowserRouter>
+        
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            
+        </Routes>
+
+        </BrowserRouter>
+    )
+}
+
+export default AppRouting;
